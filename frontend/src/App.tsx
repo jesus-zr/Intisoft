c6611fb833373e9e37c076737c27cbb82b215aeb
jesus-zr <jesus.a.zr2310@gmail.com>
2025-11-12 16:32:16 -0500

    Primera Configuración

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        🚀 Tailwind está funcionando!
+      </h1>
+      <button className="px-6 py-3 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition">
+        Probar botón
+      </button>
+    </div>
+  )
+}
+
+export default App
